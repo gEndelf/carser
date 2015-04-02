b9ee55c86b48b559a052db3bf7955e3dbb7dc9ff
gEndelf <alex.com.ua@gmail.com>
2015-04-02 09:30:12 +0300

    initial commit

---
+++ autoria/settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+BOT_NAME = 'autoria'
+
+SPIDER_MODULES = ['autoria.spiders']
+NEWSPIDER_MODULE = 'autoria.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'autori (+http://www.yourdomain.com)'
