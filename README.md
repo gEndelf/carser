6c8a81a6245107126534071aad6764bd7437b88d
gEndelf <alex.com.ua@gmail.com>
2015-04-04 23:04:28 +0300

    release 0.1

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Auto-RIA parser
 
 Usage:
+------
 
-```scrapy crawl auto.ria.com -o items.csv```
+```scrapy crawl auto.ria.com -o cars.csv -a city=[city id] -a state=[state id]```
+
+**city id** - city ID from auto.ria.ua
+**state id** - region ID from auto.ria.ua
+
+Configuration:
+--------------
+
+* **AutoRiaSpider.PARSE_URL** - change search criteria
+* **AutoRiaSpider.allowed_models** - (if defined) searching ONLY by this models 
+* **AutoRiaSpider.exclude_models** - (if defined) exclude this models
+* Results will be saved to **cars.csv**
+* more details by app structure & customization: http://scrapy.org
--- autoria/spiders/auto_ria.py
+++ autoria/spiders/auto_ria.py
@@ -21,19 +21,35 @@ from autoria.spiders import AJAX_HEADERS
 class AutoRiaSpider(scrapy.Spider):
     name = 'auto.ria.com'
     allowed_domains = ['auto.ria.com']
-    # allowed_models = ['kia', 'ford', 'renault', 'hyundai', 'skoda', 'seat', 'peugeot', 'dacia', 'volkswagen', 'mitsubishi']
+
+    allowed_models = []
     exclude_models = [u'газ', u'ваз', u'заз', u'daewoo', u'samand',
                       u'chevrolet', u'byd', u'geely', u'great wall', u'богдан',
                       u'chery']
     start_urls = ("http://auto.ria.com/",)
 
     CAR_URL = 'http://auto.ria.com/blocks_search/view/auto/{}/?lang_id=2&view_type_id=0&price_for_hot=40&price_for_fishka=1&strategy=default&domain_zone=com&user_id=0'
-    PARSE_URL = "http://auto.ria.com/blocks_search_ajax/search/?countpage=10&category_id=1&view_type_id=0&page={page}&marka=0&model=0&s_yers=2005&po_yers=0&state=7&city=7&price_ot=5000&price_do=9001&currency=1&gearbox=1&type=0&drive_type=0&door=0&color=0&metallic=0&engineVolumeFrom=&engineVolumeTo=&raceFrom=5&raceTo=70&powerFrom=&powerTo=&power_name=1&fuelRateFrom=&fuelRateTo=&fuelRatesType=city&custom=1&damage=1&saledParam=2&under_credit=0&confiscated_car=0&auto_repairs=2&with_exchange=0&with_real_exchange=0&exchangeTypeId=0&with_photo=1&with_video=0&is_hot=0&vip=0&checked_auto_ria=0&top=0&order_by=0&hide_black_list=0&auto_id=&deletedAutoSearch=0&user_id=0&scroll_to_auto_id=0&expand_search=0&can_be_checked=0&last_auto_id=0&matched_country=-1&seatsFrom=&seatsTo=&wheelFormulaId=0&axleId=0&carryingTo=&carryingFrom=&search_near_states=0&company_id=0&company_type=0&__={rnd}"
-    REFERER_URL = "http://auto.ria.com/search/?countpage=10&category_id=1&view_type_id=0&page={page}&marka=0&model=0&s_yers=2005&po_yers=0&state=7&city=7&price_ot=5000&price_do=9001&currency=1&gearbox=1&type=0&drive_type=0&door=0&color=0&metallic=0&engineVolumeFrom=&engineVolumeTo=&raceFrom=5&raceTo=70&powerFrom=&powerTo=&power_name=1&fuelRateFrom=&fuelRateTo=&fuelRatesType=city&custom=1&damage=1&saledParam=2&under_credit=0&confiscated_car=0&auto_repairs=2&with_exchange=0&with_real_exchange=0&exchangeTypeId=0&with_photo=1&with_video=0&is_hot=0&vip=0&checked_auto_ria=0&top=0&order_by=0&hide_black_list=0&auto_id=&deletedAutoSearch=0&user_id=0&scroll_to_auto_id=0&expand_search=0&can_be_checked=0&last_auto_id=0&matched_country=-1&seatsFrom=&seatsTo=&wheelFormulaId=0&axleId=0&carryingTo=&carryingFrom=&search_near_states=0&company_id=0&company_type=0"
+    PARSE_URL = "http://auto.ria.com/blocks_search_ajax/search/?countpage=10&category_id=1&view_type_id=0&page={page}&marka=0&model=0&s_yers=2005&po_yers=0&state=%(state)s&city=%(city)s&price_ot=5000&price_do=9001&currency=1&gearbox=1&type=0&drive_type=0&door=0&color=0&metallic=0&engineVolumeFrom=&engineVolumeTo=&raceFrom=5&raceTo=70&powerFrom=&powerTo=&power_name=1&fuelRateFrom=&fuelRateTo=&fuelRatesType=city&custom=1&damage=1&saledParam=2&under_credit=0&confiscated_car=0&auto_repairs=2&with_exchange=0&with_real_exchange=0&exchangeTypeId=0&with_photo=1&with_video=0&is_hot=0&vip=0&checked_auto_ria=0&top=0&order_by=0&hide_black_list=0&auto_id=&deletedAutoSearch=0&user_id=0&scroll_to_auto_id=0&expand_search=0&can_be_checked=0&last_auto_id=0&matched_country=-1&seatsFrom=&seatsTo=&wheelFormulaId=0&axleId=0&carryingTo=&carryingFrom=&search_near_states=0&company_id=0&company_type=0&__={rnd}"
+    REFERER_URL = "http://auto.ria.com/search/?countpage=10&category_id=1&view_type_id=0&page={page}&marka=0&model=0&s_yers=2005&po_yers=0&state=%(state)s&city=%(city)s&price_ot=5000&price_do=9001&currency=1&gearbox=1&type=0&drive_type=0&door=0&color=0&metallic=0&engineVolumeFrom=&engineVolumeTo=&raceFrom=5&raceTo=70&powerFrom=&powerTo=&power_name=1&fuelRateFrom=&fuelRateTo=&fuelRatesType=city&custom=1&damage=1&saledParam=2&under_credit=0&confiscated_car=0&auto_repairs=2&with_exchange=0&with_real_exchange=0&exchangeTypeId=0&with_photo=1&with_video=0&is_hot=0&vip=0&checked_auto_ria=0&top=0&order_by=0&hide_black_list=0&auto_id=&deletedAutoSearch=0&user_id=0&scroll_to_auto_id=0&expand_search=0&can_be_checked=0&last_auto_id=0&matched_country=-1&seatsFrom=&seatsTo=&wheelFormulaId=0&axleId=0&carryingTo=&carryingFrom=&search_near_states=0&company_id=0&company_type=0"
+
+    def __init__(self, name=None, **kwargs):
+        super(AutoRiaSpider, self).__init__(name, **kwargs)
+
+        assert kwargs.get('city')
+        assert kwargs.get('state')
+
+        self.city = kwargs['city']
+        self.state = kwargs['state']
+
+        self._PARSE_URL = self.PARSE_URL % {"city": self.city,
+                                            "state": self.state}
+
+        self._REFERER_URL = self.REFERER_URL % {"city": self.city,
+                                                "state": self.state}
 
     def parse(self, response):
         yield Request(
-            url=self.PARSE_URL.format(page=0, rnd=random.randint(0, 2000000)),
+            url=self._PARSE_URL.format(page=0, rnd=random.randint(0, 2000000)),
             headers=AJAX_HEADERS,
             callback=self.search_items)
 
@@ -46,11 +62,11 @@ class AutoRiaSpider(scrapy.Spider):
             return
 
         headers = AJAX_HEADERS.copy()
-        headers['Referer'] = self.REFERER_URL.format(page=0)
+        headers['Referer'] = self._REFERER_URL.format(page=0)
 
-        yield Request(url=self.PARSE_URL.format(page=page_num + 1,
-                                                rnd=random.randint(0,
-                                                                   2000000)),
+        yield Request(url=self._PARSE_URL.format(page=page_num + 1,
+                                                 rnd=random.randint(0,
+                                                                    2000000)),
                       headers=headers, callback=self.search_items)
 
         for uid in ids:
@@ -61,7 +77,6 @@ class AutoRiaSpider(scrapy.Spider):
     def parse_item(self, response):
         base_url = get_base_url(response)
         selector = Selector(response)
-        # selector = Selector(text=response.body.decode('utf-8'))
         loader = AutoItemLoader(selector=selector)
 
         uid = response.meta['uid']
@@ -70,7 +85,14 @@ class AutoRiaSpider(scrapy.Spider):
 
         title = TakeFirst()(selector.xpath(
             '//a[@nav_mem="{}"]/@title'.format(uid)).extract()).lower()
-        if any(model in title for model in self.exclude_models):
+
+        if self.allowed_models and \
+                not any(model in title for model in self.allowed_models):
+            self.log(u'It\'s not we are looking for: {}'.format(title))
+            return
+
+        if self.exclude_models and \
+                any(model in title for model in self.exclude_models):
             self.log(u'Excluded: {}'.format(title))
             return
 
